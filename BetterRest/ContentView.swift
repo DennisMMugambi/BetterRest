99271593921de39c20a4e30bbd5c46dfa98aba41
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 09:29:55 +0300

    creates basic home layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,37 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var agreedToTerms = false
-    @State private var agreedToPrivacyPolicy = false
-    @State private var agreedToEmails = false
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 
     var body: some View {
-        let agreedToAll = Binding<Bool>(
-            get: {
-                agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
-            },
-            set: {
-                agreedToTerms = $0
-                agreedToPrivacyPolicy = $0
-                agreedToEmails = $0
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
             }
-        )
-
-        return VStack {
-            Toggle("Agree to terms", isOn: $agreedToTerms)
-                .padding(.leading)
-                .padding(.trailing)
-            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
-                .padding(
-                    .leading)
-                .padding(.trailing)
-            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
-                .padding(.leading)
-                .padding(.trailing)
-            Toggle("Agree to all", isOn: agreedToAll)
-                .padding(.leading)
-                .padding(.trailing)
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
