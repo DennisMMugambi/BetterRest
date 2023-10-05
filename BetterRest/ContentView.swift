1f8c13ce8a1914015fca0c74bff2ec4e068db67d
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 10:11:49 +0300

    adds logic to make prediction from model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Dennis Mutwiri Mugambi on 03/10/2023.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
 
     var body: some View {
         NavigationView {
@@ -36,7 +41,23 @@ struct ContentView: View {
     }
     
     func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
+        showingAlert = true
     }
 }
 
